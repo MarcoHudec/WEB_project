5a8cea6da3622e264cb9c8300d45045263280c10
Masenku <marcel.ivanic@gmx.at>
2023-11-04 12:06:14 +0100

    modified Footer and Login page

--- includes/Footer.php
+++ includes/Footer.php
@@ -1,5 +1,5 @@
 <div class="container" bis_skin_checked="1">
-  <footer class="footer fixed-bottom py-0 my-0" style="background-color: white;">
+  <footer class="footer sticky-bottom py-0 my-0" style="background-color: white;">
     <ul class="nav justify-content-center border-bottom pb-1 mb-2">
       <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
       <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Gallery</a></li>
