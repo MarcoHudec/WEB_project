9775d76cd5837132f86d918255e0f99bfbc30cf5
MarcoHudec <marco.hudec3@gmail.com>
2023-11-18 18:09:42 +0100

    file (picture) finally working

---
+++ Career.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Career Page</title>
+    <?php include("includes/head.php")?>
+</head>
+
+<body>
+    
+<?php include("includes/Navbar.php")?>
+
+    <h1>Upload Images</h1>
+
+    <!-- Form for file upload -->
+    <form action="logic/file_upload.php" method="POST" enctype="multipart/form-data">
+        <div class="mb-3">
+            
+            <input type="file" name="file" accept="image/*">
+        </div>
+        <button type="submit" name="submit">Upload Image</button>
+    </form>
+
+    <h2>Uploaded Images</h2>
+    <div class="image-gallery">
+        <?php
+        // Display uploaded images
+        $directory = 'news/'; // Relative path to the directory
+        $images = scandir($directory);
+
+        foreach ($images as $image) {
+            if ($image != "." && $image != "..") {
+                echo '<img src="' . $directory . $image . '" alt="' . $image . '" style="width: 200px; height: 150px; margin: 10px;">';
+            }
+        }
+        ?>
+    </div>
+
+    <?php include("includes/Footer.php")?>
+
+    <?php include("includes/scripts.php")?>
+
+</body>
+
+</html>
--- logic/file_upload.php
+++ logic/file_upload.php
@@ -1,49 +1,75 @@
 <?php
-$targetDir = "C:/xampp/htdocs/WEB_project/news/";
+if (isset($_POST["submit"])) {
+    $file = $_FILES['file'];
 
-if(isset($_FILES["image"])) {
-    $targetFile = $targetDir . basename($_FILES["image"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
 
-    // Check if image file is a actual image or fake image
-    $check = getimagesize($_FILES["image"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
+    $fileExt = explode('.', $fileName );
+    $fileActualExt = strtolower(end($fileExt));
 
-    // Check if file already exists
-    if (file_exists($targetFile)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
+    $allowed = array('jpg', 'jpeg', 'png', 'pdf', 'docx');
 
-    // Check file size
-    if ($_FILES["image"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    } else {
-        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-            echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
+    if(in_array($fileActualExt, $allowed)){
+        if($fileError === 0) 
+            {
+                if($fileSize < 102400){
+                    $fileNameNew= uniqid('', true).".".$fileActualExt;
+                    $fileDestination = '../news/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    header("Location: ../Career.php?uploadsuccess");
+                }
+                else{
+                    echo "Your File is too big";
+                }
+        }
+        else{
+            echo "There was an error uploading your file";
         }
     }
+    else{
+        echo "You cannot upload files of this type";
+    }
+
+} ?>
+
+
+
+<?php
+session_start();
+if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
 }
 ?>
+
+<body>
+    <h1>Upload Images</h1>
+
+    <!-- Form for file upload -->
+    <form action="logic/file_upload.php" method="POST" enctype="multipart/form-data">
+        <div class="mb-3">
+            <label for="file" class="form-label">File</label>
+            <input type="file" name="file" accept="image/*">
+        </div>
+        <button type="submit" name="submit">Upload Image</button>
+    </form>
+
+    <h2>Uploaded Images</h2>
+    <div class="image-gallery">
+        <?php
+        // Display uploaded images
+        $directory = 'news/'; // Relative path to the directory
+        $images = scandir($directory);
+
+        foreach ($images as $image) {
+            if ($image != "." && $image != "..") {
+                echo '<img src="' . $directory . $image . '" alt="' . $image . '" style="width: 200px; height: 150px; margin: 10px;">';
+            }
+        }
+        ?>
+    </div>
+</body>
