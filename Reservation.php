9dbf39a48717a7e6cbf251d2b6fe8230ab98a254
Masenku <marcel.ivanic@gmx.at>
2023-11-20 15:41:34 +0100

    Reservation added

---
+++ Reservation.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Reservation</title>
+    <?php include("includes/head.php")?>
+</head>
+<body>
+    <?php include("includes/Navbar.php")?>
+        
+    <div class="container py-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="Images/HotelRoom1.jpeg" class="img-fluid rounded img-hover" alt="Image 1">
+                    <p>Single Deluxe Room with sea view</p>
+                    <a href="#" class="btn btn-primary">Book Now</a>
+                </div>
+                <div class="col-md-4">
+                    <img src="Images/HotelRoom2.jpeg" class="img-fluid rounded img-hover" alt="Image 2">
+                    <p>Family Deluxe Room with sea view</p>
+                    <a href="#" class="btn btn-primary">Book Now</a>
+                </div>
+                <div class="col-md-4">
+                    <img src="Images/HotelRoom3.jpeg" class="img-fluid rounded img-hover" alt="Image 3">
+                    <p>Pesidential sweet with sea view and complementary butler</p>
+                    <a href="" class="btn btn-primary">Book Now</a>
+                </div>
+            </div>
+        </div>
+        
+    <?php include("includes/Footer.php")?>
+    <?php include("includes/scripts.php")?>
+</body>
+
+</html>
