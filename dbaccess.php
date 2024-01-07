cd75b93502773caf42074332e5fb95b92cce6419
Masenku <marcel.ivanic@gmx.at>
2024-01-07 13:10:01 +0100

    modified dbacess

--- dbaccess.php
+++ dbaccess.php
@@ -11,27 +11,3 @@ if($db->connect_error) {
     echo "Connection Error: " . $db -> connect_error;
     exit();
 }
-
-function findAllNews() {
-    global $db;
-    
-    $sql = "SELECT * FROM news";
-    $result = $db->query($sql);
-
-    $news = [];
-    while($row = $result->fetch_array()) {
-        $news[] = $row;
-    }
-
-    return $news;
-}
-
-function saveNews($bild_url, $title, $text) {
-    global $db;
-
-    $sql = "INSERT INTO `news`(`bild_url`, `title`, `text`) VALUES (?, ?, ?)"; 
-    $stmt = $db->prepare($sql);
-    $stmt->bind_param("sss",$bild_url, $title, $text);
-
-    $stmt->execute();
-}
