c11fe3434838b0460883bfcc999c4cd66a8e6608
Masenku <marcel.ivanic@gmx.at>
2024-01-07 13:32:46 +0100

    modified profile

--- Profile.php
+++ Profile.php
@@ -15,7 +15,6 @@
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-xl-10">
                     <div class="card rounded-3 text-black">
-                        <div class="row g-0">
                             <div class="col-lg-6">
                                 <div class="card-body p-md-5 mx-md-4">
                                     <div class="text-center">
@@ -99,7 +98,6 @@
                             </div>
                             
                             
-                        </div>
                     </div>
                 </div>
             </div>
